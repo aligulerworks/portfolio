aaae2a3f40be67b271b3400bf2f8c7f4514a972f
aligulerworks <aligulerworks@gmail.com>
2023-11-23 11:08:13 +0300

    Some Fixes

--- components/head-tag-home.js
+++ components/head-tag-home.js
@@ -8,33 +8,19 @@ const headTagHome = `
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Inconsolata:wght@200;300;400;700;900&family=Ysabeau+Office:ital,wght@0,1;0,100;0,200;0,400;0,700;0,900;0,1000;1,1;1,100;1,200;1,400;1,700;1,900;1,1000&display=swap" rel="stylesheet">
 
-  <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-SCJSHQW09H"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-SCJSHQW09H');
-</script>
-<!-- END - Google tag (gtag.js) -->
-
 
   <!-- favicon  -->
   <link rel="icon" type="image/svg+xml" href="/images/favicon-alidelvento.svg" />
 
   <!-- Google Tag Manager -->
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-SCJSHQW09H"></script>
   <script>
-    (function (w, d, s, l, i) {
-      w[l] = w[l] || [];
-      w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-      var f = d.getElementsByTagName(s)[0],
-        j = d.createElement(s),
-        dl = l != 'dataLayer' ? '&l=' + l : '';
-      j.async = true;
-      j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-      f.parentNode.insertBefore(j, f);
-    })(window, document, 'script', 'dataLayer', 'GTM-MDTH6X7');
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+  
+    gtag('config', 'G-SCJSHQW09H');
   </script>
   <!-- End Google Tag Manager -->
 
@@ -43,7 +29,7 @@ const headTagHome = `
   <link rel="stylesheet" href="./style.css" />
 
 
-  <title>ali del vento</title>
+  <title>Ali Güler Works</title>
 `;
 
 // document.getElementsByTagName("head")[0].appendChild(headTagHome);
